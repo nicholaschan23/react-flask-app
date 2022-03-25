818bfd3338ec45a6d630c8014f782a92f53c82ca
Nick <nick.chan@utexas.edu>
2022-03-25 17:36:45 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(__name__, static_folder='./python-flask-app/build', static_url_path='/') # where static files are stored
+app = Flask(__name__, static_folder='./react-flask-app/build', static_url_path='/') # where static files are stored
 # static_url_path is seen in front end, static_folder seen in backend 
 
 @app.route('/')
