f51358a47346468d985e88720ae484ef0bc99a04
Nick <nick.chan@utexas.edu>
2022-03-25 23:27:54 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify
 
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__, static_folder="./react-flask-app/build", static_url_path='/') # where static files are stored
 # static_url_path is seen in front end, static_folder seen in backend
 
-CORS(app)
+# CORS(app)
 
 @app.route('/')
 def index():
